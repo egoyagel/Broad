c2f8ca6ee88d9b616383e9f8547ec7cbc3b24743
egoyagel <egoyagel@gmail.com>
2023-07-05 08:34:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-DB_URL = os.environ.get("DB_URL", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6116735221:AAGAsurz-k1TR43xwk3CWbiQfu0gaRoWiLs")
+API_ID = int(os.environ.get("API_ID", "23776178"))
+API_HASH = os.environ.get("API_HASH", "b69598ab1d4021eb968d2db5b8bed30a")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001984763255"))
+AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1205219610").split())
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://ego:Egobeyler99@cluster0.p00hwbq.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
 BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
